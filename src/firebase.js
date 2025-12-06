3634df9bfc19ad3cbdfffbea7ba0fcf908a00e7c
wanzHOSTING-code <jajanghermawan0812@gmail.com>
2025-12-06 13:41:28 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDNZm2wnMwTiBDc6Q6bSHETTqyMK76BvLQ",
@@ -11,4 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+// ✅ INI YANG SERING KURANG
 export const db = getFirestore(app);
+export const storage = getStorage(app);
